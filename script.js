7f2f984e40aadfa99372ca531ffbac80e0998174
dawood1123 <davidkhan1226@gmail.com>
2024-09-01 12:00:41 -0700

    questions added

--- script.js
+++ script.js
@@ -3,34 +3,61 @@ const quiz = [
     {
         question: "What is abriviation of JS?",
         options: ["Java script", "Java", "Script Java", "None of these"],
-        answer: "Paris"
+        answer: "0"
     },
     {
         question: "What is 2 + 2?",
         options: ["3", "4", "5", "6"],
-        answer: "4"
+        answer: "1"
     },
     {
         question: "What is the largest planet in our solar system?",
         options: ["Earth", "Jupiter", "Mars", "Saturn"],
-        answer: "Jupiter"
+        answer: "1"
     },
     {
         question: "Who is the founder of Pakistan",
         options: ["Allama Iqbal", "Muhammad Ali Jinnah", "Fatima Jinnah", "Liaqat Ali Khan"],
-        answer: "Harper Lee"
+        answer: "1"
     },
     {
         question: "What is the chemical symbol for gold?",
         options: ["Au", "Ag", "Pb", "Fe"],
-        answer: "Au"
+        answer: "0"
     }
 ];
 
 var score = 0; 
+var quenum = 0; 
 
-var currentQuestion = document.getElementById("que")
-currentQuestion.textContent = quiz[1].question;
+function que(quenum){
 
-var currentOption = document.getElementById("opt-btn")
-currentOption.textContent = quiz[1].options;
+    var currentQuestion = document.getElementById("que")
+currentQuestion.textContent = quiz[quenum].question;
+
+var currentOption = document.getElementById("btn1")
+currentOption.textContent = quiz[quenum].options[0];
+
+var currentOption = document.getElementById("btn2")
+currentOption.textContent = quiz[quenum].options[1];
+
+var currentOption = document.getElementById("btn3")
+currentOption.textContent = quiz[quenum].options[2];
+
+var currentOption = document.getElementById("btn4")
+currentOption.textContent = quiz[quenum].options[3];
+
+}
+
+que(quenum);
+var score; 
+
+function selectOption(selectedIndex) {
+    var correctAnswerIndex = quiz[quenum].answer;
+    if (selectedIndex === correctAnswerIndex) {
+        score++;
+    }
+}
+
+var currentscore = document.getElementById("score")
+currentscore.textContent = score; 
